97e6331e654d1ab7716b1afe772159e4cf446d72
Joao Brigido <jv_brigido@hotmail.com>
2024-08-02 12:40:33 -0300

    exercise: Solve the Distance problem of Beecrowd

---
+++ Beecrowd/Distance/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    int distance = int.Parse(Console.ReadLine());
+
+    int time = distance * 2;
+
+    Console.WriteLine($"{time} minutos");
+
+
+  }
+
+}
