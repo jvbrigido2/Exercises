897326ea9663520f6d8351d451d725b9b738bfb4
Joao Brigido <jv_brigido@hotmail.com>
2024-08-02 12:48:34 -0300

    exercise: Solve the FuelSpent problem of Beecrowd

---
+++ Beecrowd/FuelSpent/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+    int spentTime = int.Parse(Console.ReadLine());
+    int averageSpeed = int.Parse(Console.ReadLine());
+
+    int distance = spentTime * averageSpeed;
+
+    double totalFuelSpent = distance / 12.0;
+
+    Console.WriteLine($"{totalFuelSpent:F3}");
+
+  }
+
+}
