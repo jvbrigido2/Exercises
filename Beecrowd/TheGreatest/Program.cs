b0a31fe5d455b33e19285454890ea1efc4191a2c
Joao Brigido <jv_brigido@hotmail.com>
2024-08-02 11:54:48 -0300

    exercise: Solve TheGreatest problem of Beecrowd

---
+++ Beecrowd/TheGreatest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+    int greatest;
+    string[] inputs = Console.ReadLine().Split(' ');
+    int firstNumber = int.Parse(inputs[0]);
+    int secondNumber = int.Parse(inputs[1]);
+    int thirdNumber = int.Parse(inputs[2]);
+
+    greatest = Greatest(firstNumber, secondNumber);
+    greatest = Greatest(greatest, thirdNumber);
+
+    Console.WriteLine($"{greatest} eh o maior");
+
+  }
+
+  static int Greatest(int number1, int number2)
+  {
+    return (number1 + number2 + Math.Abs(number1 - number2)) / 2;
+  }
+
+}
