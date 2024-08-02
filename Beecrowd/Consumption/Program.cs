5fe6dd5a2dcb7054ed4941592fd4e89ecb28b255
Joao Brigido <jv_brigido@hotmail.com>
2024-08-02 12:02:15 -0300

    exercise: Solve the Consumption problem of Beecrowd

---
+++ Beecrowd/Consumption/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+    int totalDistance = int.Parse(Console.ReadLine());
+    double spentFuel = double.Parse(Console.ReadLine());
+    double average = totalDistance / spentFuel;
+
+    Console.WriteLine($"{average:F3} km/l");
+
+
+
+
+  }
+
+}
