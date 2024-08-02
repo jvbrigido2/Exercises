b4fd140a452480a283b3fb16a153fb878f2c1274
Joao Brigido <jv_brigido@hotmail.com>
2024-08-02 12:12:11 -0300

    exercise: Solve the DistanceBetweenTwoPoints problem of Beecrowd

---
+++ Beecrowd/DistanceBetweenTwoPoints/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    string[] inputs1 = Console.ReadLine().Split(' ');
+    double x1 = double.Parse(inputs1[0]);
+    double y1 = double.Parse(inputs1[1]);
+
+    string[] inputs2 = Console.ReadLine().Split(' ');
+    double x2 = double.Parse(inputs2[0]);
+    double y2 = double.Parse(inputs2[1]);
+
+    double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+
+    Console.WriteLine($"{distance:F4}");
+
+  }
+
+}
