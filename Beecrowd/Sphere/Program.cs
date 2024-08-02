1ec4058f63ec78d5d17645eddf341e9f8341d5ec
Joao Brigido <jv_brigido@hotmail.com>
2024-08-02 11:19:34 -0300

    exercise: Solve Sphere of Beecrowd

---
+++ Beecrowd/Sphere/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    const double PI = 3.14159;
+    double radius = double.Parse(Console.ReadLine());
+
+
+    double sphere = (4 / 3.0) * PI * Math.Pow(radius, 3);
+
+    Console.WriteLine($"VOLUME = {sphere:F3}");
+
+  }
+
+}
