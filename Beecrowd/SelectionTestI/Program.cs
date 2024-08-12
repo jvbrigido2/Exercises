3f8847c14c8335d237bdcd84931e6cd0a29e28fb
Joao Brigido <jv_brigido@hotmail.com>
2024-08-12 13:41:45 -0300

    exercise: Solve the SelectionTestI of Beecrowd

---
+++ Beecrowd/SelectionTestI/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    string[] inputs = Console.ReadLine().Split(' ');
+
+    int A, B, C, D;
+
+    A = int.Parse(inputs[0]);
+    B = int.Parse(inputs[1]);
+    C = int.Parse(inputs[2]);
+    D = int.Parse(inputs[3]);
+
+    if ((B > C && D > A) && (C + D > A + B) && C > 0 && D > 0 && (A % 2 == 0))
+    {
+      Console.WriteLine("Valores aceitos");
+    }
+    else
+    {
+      Console.WriteLine("Valores nao aceitos");
+    }
+
+
+  }
+
+}
