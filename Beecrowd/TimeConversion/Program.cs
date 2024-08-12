5e88cff3d71f2d9c4bba5f4bf90bc4130b83dc03
Joao Brigido <jv_brigido@hotmail.com>
2024-08-12 12:23:40 -0300

    exercise: Solve the TimeConversion of Beecrowd

---
+++ Beecrowd/TimeConversion/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    int input = int.Parse(Console.ReadLine());
+
+    int hours = input / 3600;
+
+    int minutes = (input % 3600) / 60;
+
+    int seconds = (input % 3600) % 60;
+
+    Console.WriteLine($"{hours}:{minutes}:{seconds}");
+
+
+
+  }
+
+}
