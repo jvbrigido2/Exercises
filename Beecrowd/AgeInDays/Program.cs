ee205aa0cc44a8dca82dbe28dd1345b11e9a1b32
Joao Brigido <jv_brigido@hotmail.com>
2024-08-12 12:28:51 -0300

    exercise: Solve the AgeInDays of Beecrowd

---
+++ Beecrowd/AgeInDays/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class URI
+{
+
+  static void Main(string[] args)
+  {
+
+    int ageInDays = int.Parse(Console.ReadLine());
+
+    int year = ageInDays / 365;
+    int month = (ageInDays % 365) / 30;
+    int day = (ageInDays % 365) % 30;
+
+    Console.WriteLine($"{year} ano(s)");
+    Console.WriteLine($"{month} mes(es)");
+    Console.WriteLine($"{day} dia(s)");
+  }
+
+}
